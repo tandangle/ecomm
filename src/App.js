136a1f12af705717fb75a5b979b8747b1f1f2ac8
Tandangle <tandangle76@gmail.com>
2020-07-05 22:47:19 -0400

    first commit, works with hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { Elements, StripeProvider } from 'react-stripe-elements';
+import items from './api/products';
+import Product from './components/Product';
+import Cart from './components/Cart';
+// import CheckoutForm from './components/CheckoutForm/CheckoutForm';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+	const [ itemsInCart, setItemsInCart ] = useState([]);
 
-export default App;
+	const handleAddToCartClick = (id) => {
+		setItemsInCart((itemsInCart) => {
+			const itemInCart = itemsInCart.find((item) => item.id === id);
+
+			// if item is already in cart, update the quantity
+			if (itemInCart) {
+				return itemsInCart.map((item) => {
+					if (item.id !== id) return item;
+					return { ...itemInCart, quantity: item.quantity + 1 };
+				});
+			}
+
+			// otherwise, add new item to cart
+			const item = items.find((item) => item.id === id);
+			return [ ...itemsInCart, { ...item, quantity: 1 } ];
+		});
+	};
+
+	const totalCost = itemsInCart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1 className="App-header-text">Tan's Shop</h1>
+			</header>
+			<main className="App-shop">
+				<div className="App-products">
+					{items.map((item) => (
+						<Product
+							key={item.title}
+							title={item.title}
+							price={item.price}
+							onAddToCartClick={() => handleAddToCartClick(item.id)}
+						/>
+					))}
+				</div>
+				<Cart itemsInCart={itemsInCart} totalCost={totalCost} />
+			</main>
+		</div>
+	);
+}
---
+++ src/api/products.js
@@ -0,0 +1,7 @@
+export default [
+	{ id: 1, title: 'Cumin - Ground', price: 68.42, ' category': 'Baby' },
+	{ id: 2, title: 'Lemonade - Black Cherry, 591 Ml', price: 40.58, ' category': 'Kids' },
+	{ id: 3, title: 'Rice - Sushi', price: 18.79, ' category': 'Kids' },
+	{ id: 4, title: 'Jerusalem Artichoke', price: 36.53, ' category': 'Grocery' },
+	{ id: 5, title: 'Nut - Chestnuts, Whole', price: 87.16, ' category': 'Garden' }
+];
